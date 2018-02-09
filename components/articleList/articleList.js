2cf144ea5d49e7579a4b3de471a89f31ec328cb2
taomingwang <taojike@126.com>
2018-02-09 14:49:30 +0800

    列表组件化

---
+++ components/articleList/articleList.js
@@ -0,0 +1,29 @@
+// components/articleList/articleList.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    articleDataList: {
+      type: Array,
+      value: [],
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onTap: function () {
+      var myEventDetail = {} // detail对象，提供给事件监听函数
+      var myEventOption = {} // 触发事件的选项
+      this.triggerEvent('myOnTap', myEventDetail, myEventOption)
+    },
+  }
+})
