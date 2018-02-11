553618655a21ac65ce4b0c20f96aaab89b8cc35f
taomingwang <taojike@126.com>
2018-02-11 16:36:54 +0800

    自动跳转

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,6 @@ Page({
     motto: '专注打脸20年',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function () {
@@ -23,18 +22,23 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      that.openNextPageDelay();
     } 
-    else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res,
-          hasUserInfo: true
-        })
-      }
+  },
+  /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+  onReady: function () {
+    let that = this;   
+    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    // 所以此处加入 callback 以防止这种情况
+    app.userInfoReadyCallback = res => {
+      this.setData({
+        userInfo: res,
+        hasUserInfo: true
+      })
+      that.openNextPageDelay();
     }
-
   },
   getUserInfo: function (e) {
     let that = this;
@@ -45,6 +49,21 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
+      that.openNextPageDelay();
     }).catch(console.error);
+  },
+  openNextPageDelay:function() {
+    let that = this;
+    setTimeout(function () {
+      that.setData({
+        motto: '正在跳转……'
+      })
+    }, 1000);
+    
+    setTimeout(function () {
+      wx.reLaunch({
+        url: '../mainlist/mainlist',
+      })
+    }, 2000);
   }
 })
